e5eecbd19c7935f19638c8d916118979acb2775e
Altun0 <91114236+Altun0@users.noreply.github.com>
2021-11-29 20:11:38 +0400

    new commit

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-function Main(arr){
-    let evenarr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]%2==0) {
-            evenarr.push(arr[i]);
-        }
+function Reverse(arry){
+    let newarr = []
+    for (let i = arry.length - 1; i >= 0; i--) {
+        newarr+=arry[i];
     }
-    return evenarr
+    return newarr ;
 }
-console.log(Main([1,2,3,4,5,6,7,8,9]));
-
-
-
-
-
-function calculator(num1,num2,math){
-    switch (math) {
-        case '+':
-            return num1+num2;
-            break;
-        case '-':
-            return num1-num2;
-             break;
-        case '*':
-            return num1*num2;
-            break;
-        case '/':
-            return num1/num2;
-            break; 
-        default:
-            console.log("error");
-            break;
-    }
-}
-console.log(calculator(40,2,'+'));
-
-
-
-function bool(num){
-     let newarr=[2,23,4,5,6,67,9]
-
-    for (let i = 0; i < newarr.length; i++) {
-        if (newarr[i]==num) {
-            return true
-        }
-    }
-    return false
-}
-console.log(bool(230));
+console.log(Reverse("hello mello hi"));
